b3e50efe2678c8c65f18fcdeea590166ffeca8a7
d pullaiah <pullaiah.dudekula@improve10x.com>
2022-12-30 19:40:22 +0530

    resolved the conflicts in package name

--- app/src/main/java/com/improve10x/pomodoro/SettingsActivity.java
+++ app/src/main/java/com/improve10x/pomodoro/SettingsActivity.java
@@ -1,11 +1,11 @@
-package com.example.pomodoro;
+package com.improve10x.pomodoro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.SeekBar;
 
-import com.example.pomodoro.databinding.ActivitySettingsBinding;
+import com.improve10x.pomodoro.databinding.ActivitySettingsBinding;
 
 public class SettingsActivity extends AppCompatActivity {
     private ActivitySettingsBinding binding;
