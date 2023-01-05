95ede1bfc44e233f2dab2b394c591af00791cb58
Anupama <anupama.lakshmi@Improve10x.com>
2023-01-05 17:55:06 +0530

    Implemented PomodoroActivityActionListener Interface and done with functionality of MotivationalDialogueFragment button functionality

--- app/src/main/java/com/improve10x/MotivationalDialogueFragment.java
+++ app/src/main/java/com/improve10x/MotivationalDialogueFragment.java
@@ -3,24 +3,49 @@ package com.improve10x;
 import android.os.Bundle;
 
 import androidx.fragment.app.DialogFragment;
-import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.improve10x.pomodoro.R;
 import com.improve10x.pomodoro.databinding.FragmentMotivationalDialogueBinding;
 
 
 public class MotivationalDialogueFragment extends DialogFragment {
 
     private FragmentMotivationalDialogueBinding binding;
+    private PomodoroActivityActionListener actionListener;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentMotivationalDialogueBinding.inflate(getLayoutInflater());
+        actionListener = (PomodoroActivityActionListener) getActivity();
+        handleShortBreak();
+        handleLongBreak();
+        handleSkipBreak();
         return binding.getRoot();
+    }
+
+    private void handleShortBreak() {
+        binding.startShortBreakBtn.setOnClickListener(view -> {
+            actionListener.startShortBreak();
+            dismiss();
+        });
+    }
+
+    private void handleLongBreak() {
+        binding.startLongBreakBtn.setOnClickListener(view -> {
+            actionListener.startLongBreak();
+            dismiss();
+        });
+    }
+
+    private void handleSkipBreak() {
+        binding.skipBreak.setOnClickListener(view -> {
+            actionListener.skipBreak();
+            dismiss();
+        });
     }
 }
