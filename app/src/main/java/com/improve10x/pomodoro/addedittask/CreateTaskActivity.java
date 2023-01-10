7bde1dc9d9ea91725077d2e976b9fdbcb6ce1e0a
manoj <manoj.mopoori@improve10x.com>
2023-01-10 17:14:58 +0530

    implemented progressBar in todoFragment and using group of items hide

--- app/src/main/java/com/improve10x/pomodoro/addedittask/CreateTaskActivity.java
+++ app/src/main/java/com/improve10x/pomodoro/addedittask/CreateTaskActivity.java
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnSuccessListener;
