2ed6f7bb537691f9ec3b3a56a2c6e3b066326799
Anupama <anupama.lakshmi@Improve10x.com>
2023-07-31 19:16:34 +0530

    Resolved Delete Functionality bug

--- app/src/main/java/com/improve10x/pomodoro/addedittask/TaskActivity.java
+++ app/src/main/java/com/improve10x/pomodoro/addedittask/TaskActivity.java
@@ -38,9 +38,9 @@ public class TaskActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
-                handleAddFab();
+              */  handleAddFab();
             }
         });
     }
